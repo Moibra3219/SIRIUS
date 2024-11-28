1ffc444a00c63726bd3c4683188e61a3cb72e592
Mohamed Ibrahem <mohamed.ibrahem3219@gmail.com>
2024-11-28 17:48:21 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SIRIUS | A LEADER, A GUIDER & A DIGITALIZER",
+  description: "A landing page for a marketing startup",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
